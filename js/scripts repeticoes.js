bf6f727898a73d44a9e6eaebb6e5edb9b498605b
Grazielle <graziele.f.silva9@aluno.senai.br>
2026-06-17 16:44:51 -0300

    LISTANDO  OBJETO LITERAL PELO FOR

--- js/scripts repeticoes.js
+++ js/scripts repeticoes.js
@@ -83,11 +83,18 @@ presentes.forEach((elemento, posicao)=>{
 
 //COLEÇÃO DE OBJETOS LITERAIS - ARRAY
 const pessoas = [
-  {nome: 'Maria Flor', idade: 25, renda:8500},
-  {nome: 'Joerdson Souza', idade: 75, renda:5000},
-  {nome: 'Taoca', idade: 28, renda:500},
-  {nome: 'Chicó', idade: 36, renda:100},
-  {nome: 'João Grilo', idade: 32, renda:80},
-
-
+  {nome: 'Maria Flor', idade: 25, renda: 8500},
+  {nome: 'Joerdson Souza', idade: 75, renda: 5000},
+  {nome: 'Taoca', idade: 28, renda: 500},
+  {nome: 'Chicó', idade: 36, renda: 100},
+  {nome: 'João Grilo', idade: 32, renda: 80},
 ]
+
+//LISTANDO  OBJETO LITERAL PELO FOR
+const divListaObjFor = document.querySelector('#div-listaobj-for')
+
+for(i = 0; i < pessoas.length;i++){
+  divListaObjFor.innerHTML += `${pessoas[i].nome}, - ${pessoas[i].idade}
+  R$ ${pessoas[i].renda.toFixed(2).replace('.',',')} <br>`
+
+}
